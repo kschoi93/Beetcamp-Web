b9c6ed115b0dd3d0af7cc83f37731a40d55c7217
kschoi <kyungsik0907@gmail.com>
2021-03-21 20:48:00 +0900

    test

--- WebJSP/src/TestPage.java
+++ WebJSP/src/TestPage.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.SortedSet;
@@ -6,25 +7,13 @@ import java.util.TreeSet;
 
 public class TestPage {
 	public static void main(String[] args) {
-		int[]nums = {1,1,1,5,4,6,1,3,2,2,2,2,2,3,4,5,6,7,8};
-		
-		int maxMonster = nums.length/2;
-		TreeSet<Integer> ts = new TreeSet<Integer>();
-		for(int i=0; i<nums.length;i++) {
-			ts.add(nums[i]);
-			
-		}		
-		System.out.println(ts.toString());
-		
-		for(int i=0; i<ts.size();i++) {
-		System.out.println(ts.);
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		int[] i = {1,6,5,4,5,1,6,4,3,7};
+		for(int j =0; j<i.length;j++) {
+			test.add(i[j]);
 		}
-		
-		if(maxMonster<ts.size()) {
-			//maxMonster가 더 적으면
-			System.out.println(maxMonster);
-		} else {
-			System.out.println(ts.size());
+		for(int j=0;j<i.length;j++) {
+			System.out.println(test.);
 		}
 	}
 }
