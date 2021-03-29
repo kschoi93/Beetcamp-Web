94c63ad43b65e6bf7c2174c6c1c1f498819fc102
kschoi <kyungsik0907@gmail.com>
2021-03-29 23:21:25 +0900

    create portfolio site 시작!

---
+++ WebExample/src/com/example/home/ControllerAction.java
@@ -0,0 +1,10 @@
+package com.example.home;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ControllerAction {
+	public void init(HttpRequest req, HttpResponse res) {
+		
+	}
+}
